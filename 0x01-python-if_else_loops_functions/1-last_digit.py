f914c7c4fae1eea2eaf79a17123968b4ef0d0cb7
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-11-29 09:32:36 -0800

    fix error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 number = str(number)
 if int(number[-1]) > 5:
     print(f"{number:d} is greater than 5")
-elif int(number[-1] == 0:
+elif int(number[-1]) == 0:
     print(f"{number:d} is 0")
 else:
     print(f"{number:d} is less than 6 and not 0")
