6c7692e8ad55b6532002b2a1256330ac40a0c62c
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-11-29 09:30:41 -0800

    file to check last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+number = str(number)
+if int(number[-1]) > 5:
+    print(f"{number:d} is greater than 5")
+elif int(number[-1] == 0:
+    print(f"{number:d} is 0")
+else:
+    print(f"{number:d} is less than 6 and not 0")
