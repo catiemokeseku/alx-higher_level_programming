11602b83aedc6331ab81b0321e65e1959e2734b6
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-12-07 09:21:32 -0800

    file

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in range(len(my_list)):
+        if my_list[i] == sreach:
+            my_list[i] = replace
+    return my_list
