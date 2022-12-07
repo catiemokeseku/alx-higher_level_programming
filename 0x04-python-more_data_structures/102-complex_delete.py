d20b75fec0fcc541105a03780cc269138739e98c
catiemokeseku <43410683+catiemokeseku@users.noreply.github.com>
2022-12-07 20:02:40 +0000

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, value):
+def complex_delete(a_dictionary, value):
     for key in a_dictionary:
       if a_dictionary[key] == value:
           del a_dictionary[key]
