3d1a46772cdb73fffc36ab0a69861be4d6affa4c
catiemokeseku <43410683+catiemokeseku@users.noreply.github.com>
2022-12-07 19:19:09 +0000

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     biggest = 0
     key = ""
-    if type(a_dictionary) != dict:
+    if type(a_dictionary) != dict or len(a_dictionary) == 0:
         return None
     for k, v in a_dictionary.items():
         if v > biggest:
