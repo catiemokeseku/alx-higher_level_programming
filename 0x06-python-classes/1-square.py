e26833a23d3157d5f312526f179be0cff693647c
Caleb Atiemo-Keseku <calebatiemo13@gmail.com>
2022-12-20 10:06:15 -0800

    comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Define a square class"""
 
 class Square:
 
     def __init__(self, size):
+        """Initialize a new square
+
+        Args:
+        size (int): The size of the new square
+        """
         self.__size = size
