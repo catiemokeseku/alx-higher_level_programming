bf6f4c96790ca429cf89ee7208efaa76cbca81b6
catiemokeseku <43410683+catiemokeseku@users.noreply.github.com>
2022-12-20 19:44:17 +0000

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Define a Square class"""
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+        Args:
+        size (int): The size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
+    
+    @property
+    def size(self):
+        """Get the size of the square"""
+        return self.__size
+    
+    @size.setter  
+    def size(self, value):
+        """Set the size of the square"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
